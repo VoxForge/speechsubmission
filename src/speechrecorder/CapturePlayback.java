70b9208fa20d9d64c6998b544edd50268180f717
kmaclean <kmaclean@voxforge.org>
2016-07-19 15:54:23 -0400

    refactored graphical display to its own class

--- src/speechrecorder/CapturePlayback.java
+++ src/speechrecorder/CapturePlayback.java
@@ -38,35 +38,21 @@ package speechrecorder;
  * redistribute the Software for such purposes.
  */
 
-import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.font.FontRenderContext;
-import java.awt.font.LineBreakMeasurer;
-import java.awt.font.TextAttribute;
-import java.awt.font.TextLayout;
-import java.awt.geom.Line2D;
 import java.awt.geom.Line2D.Double;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.net.URL;
-import java.nio.channels.FileChannel;
-import java.text.AttributedCharacterIterator;
-import java.text.AttributedString;
 import java.util.Calendar;
 import java.util.Vector;
 
@@ -95,6 +81,7 @@ import javax.swing.border.SoftBevelBorder;
 
 import speechrecorder.ConfigReader;
 import speechrecorder.SaveOrUpload;
+import speechrecorder.SamplingGraph;
 
 /**
  * Capture/Playback sample.  Record audio in different formats and then playback the recorded audio.  The captured audio can  be saved either as a WAVE, AU or AIFF.  Or load an audio file for streaming playback.
@@ -157,11 +144,6 @@ public class CapturePlayback extends JPanel implements ActionListener, net.sf.po
     private final File[] wavFileA = new File [numberofPrompts];
  
     private final File [] uploadWavFileA = new File [numberofPrompts];
-  
-    private File promptsFile;  
-    private File readmeFile;    
-    private File licenseFile;   
-    private File licenseNoticeFile;    
 
     JProgressBar progBar;
     int sentBytes;
@@ -412,7 +394,12 @@ public class CapturePlayback extends JPanel implements ActionListener, net.sf.po
         JPanel samplingPanel = new JPanel(new BorderLayout());
         samplingPanel.setBorder(new CompoundBorder(eb, sbb));
         
-        samplingGraph = new SamplingGraph();
+        samplingGraph = new SamplingGraph(
+        				this,
+        				lines,
+        				capture,
+        				playback
+        );
         samplingGraph.setPreferredSize(new Dimension(50, 100));
         samplingPanel.add(samplingGraph);
         
@@ -498,14 +485,6 @@ public class CapturePlayback extends JPanel implements ActionListener, net.sf.po
 				uploadWavFileA[i] = new File(tempdir + promptidA [i] + ".wav");
 				uploadWavFileA[i].deleteOnExit();
 	        }
-			//promptsFile = new File(tempdir + "prompts.txt");			
-			//promptsFile.deleteOnExit();		
-			//readmeFile = new File(tempdir + "readme.txt");			
-			//readmeFile.deleteOnExit();		
-			//licenseFile = new File(tempdir + "GPL_license.txt");			
-			//licenseFile.deleteOnExit();			
-			//licenseNoticeFile = new File(tempdir + "license.txt");	
-			//licenseNoticeFile.deleteOnExit();	
 		} catch (Exception e) {
 			System.err.println("Unable to create WAV cache file for storing audio\n" + e);
 			return;
@@ -948,7 +927,7 @@ public class CapturePlayback extends JPanel implements ActionListener, net.sf.po
                 duration = milliseconds / 1000.0;
         		System.err.println("createAudioInputStream duration:" + duration);
                 if (updateComponents) {
-                    samplingGraph.createWaveForm(null);
+                    samplingGraph.createWaveForm(audioInputStream, null);
                 }
             } catch (Exception ex) { 
                 reportStatus(ex.toString());
@@ -957,67 +936,21 @@ public class CapturePlayback extends JPanel implements ActionListener, net.sf.po
             reportStatus("Audio file required.");
         }
     }
-
-
-   public void saveToFile(String name, AudioFileFormat.Type fileType) {
-
-        if (audioInputStream == null) {
-            reportStatus("No loaded audio to save");
-            return;
-        } else if (file != null) {
-            createAudioInputStream(file, false);
-        }
-
-        // reset to the beginning of the captured data
-        try {
-            audioInputStream.reset();
-        } catch (Exception e) { 
-            reportStatus("Unable to reset stream " + e);
-            return;
-        }
-
-        File file = new File(fileName = name);
-        try {
-            if (AudioSystem.write(audioInputStream, fileType, file) == -1) {
-                throw new IOException("Problems writing to file");
-            }
-        } catch (Exception ex) { reportStatus(ex.toString()); }
-        samplingGraph.repaint();
-    }
-   
-   public void saveToFile(File file, AudioFileFormat.Type fileType) {
-
-       if (audioInputStream == null) {
-           reportStatus("No loaded audio to save");
-           return;
-       } else if (file != null) {
-           createAudioInputStream(file, false);
-       }
-
-       // reset to the beginning of the captured data
-       try {
-           audioInputStream.reset();
-       } catch (Exception e) { 
-           reportStatus("Unable to reset stream " + e);
-           return;
-       }
-
- //      File file = new File(fileName = name);
-       try {
-           if (AudioSystem.write(audioInputStream, fileType, file) == -1) {
-               throw new IOException("Problems writing to file");
-           }
-       } catch (Exception ex) { reportStatus(ex.toString()); }
-       samplingGraph.repaint();
-   }
    
     private void reportStatus(String msg) {
         if ((errStr = msg) != null) {
             System.out.println(errStr);
-            samplingGraph.repaint();
+            //samplingGraph.repaint();
+            samplingGraph.repaintGraph(
+        			audioInputStream,
+            		sampleGraphFileLabel, 
+        		    fileName,
+        			sampleGraphLengthLabel,
+        			duration,
+        			sampleGraphPositionLabel
+            );
         }
     }
-
 
     /**
      * Write data to the OutputChannel.
@@ -1041,7 +974,15 @@ public class CapturePlayback extends JPanel implements ActionListener, net.sf.po
         private void shutDown(String message) {
             if ((errStr = message) != null) {
                 System.err.println(errStr);
-                samplingGraph.repaint();
+                //samplingGraph.repaint();
+                samplingGraph.repaintGraph(
+                		audioInputStream,
+                		sampleGraphFileLabel, 
+            		    fileName,
+            			sampleGraphLengthLabel,
+            			duration,
+            			sampleGraphPositionLabel            		
+                );
             }
             if (thread != null) {
                 thread = null;
@@ -1112,8 +1053,16 @@ public class CapturePlayback extends JPanel implements ActionListener, net.sf.po
         	// debug System.err.println("outbaos size:" + outbaos.size());            
         	outbaos.reset();
         	outbaos = null;
-            samplingGraph.createWaveForm(audioBytes); 
-            samplingGraph.repaint();
+            samplingGraph.createWaveForm(audioInputStream,audioBytes); 
+            //samplingGraph.repaint();
+            samplingGraph.repaintGraph(
+            		audioInputStream,
+            		sampleGraphFileLabel, 
+        		    fileName,
+        			sampleGraphLengthLabel,
+        			duration,
+        			sampleGraphPositionLabel            		
+            );
             
 			getAudioInputStream();
             playbackInputStream = AudioSystem.getAudioInputStream(format, audioInputStream);
@@ -1203,7 +1152,15 @@ public class CapturePlayback extends JPanel implements ActionListener, net.sf.po
                 thread = null;
                 samplingGraph.stop();
                 System.err.println(errStr);
-                samplingGraph.repaint();
+                //samplingGraph.repaint();
+                samplingGraph.repaintGraph(
+                		audioInputStream,
+                		sampleGraphFileLabel, 
+            		    fileName,
+            			sampleGraphLengthLabel,
+            			duration,
+            			sampleGraphPositionLabel            		
+                );
             }
         }
 
@@ -1294,7 +1251,6 @@ public class CapturePlayback extends JPanel implements ActionListener, net.sf.po
             // load bytes into the audio input stream for playback
             getAudioInputStream();
       	
-            // saveToFile(this.uploadWavFile, AudioFileFormat.Type.WAVE);
             try {
                 if (AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, uploadWavFile) == -1) {
                     throw new IOException("Problems writing to file");
@@ -1306,7 +1262,7 @@ public class CapturePlayback extends JPanel implements ActionListener, net.sf.po
         	duration = totalBytesWritten / (double) (format.getSampleRate() * format.getSampleSizeInBits()/ 8);
     		System.err.println("capture duration:" + duration);
         	// debug	System.err.println("Calculated duration");
-            samplingGraph.createWaveForm(audioBytes);
+            samplingGraph.createWaveForm(audioInputStream, audioBytes);
             // debug System.err.println("Created samplingGraph");
             // This is the only way to "reset" long streams - re-grab
             getAudioInputStream();
@@ -1353,207 +1309,6 @@ public class CapturePlayback extends JPanel implements ActionListener, net.sf.po
 	}
 
 
-    /**
-     * Render a WaveForm.
-     */
-    class SamplingGraph extends JPanel implements Runnable {
-        private Thread thread;
-        private final Font font12 = new Font("serif", Font.PLAIN, 12);
-        Color jfcBlue = new Color(204, 204, 255);
-        Color pink = new Color(255, 175, 175);
-        protected boolean peakWarning = false;
-
-        public SamplingGraph() {
-            setBackground(new Color(20, 20, 20));
-        }
-
-        public void clearWaveForm() {
-            lines.removeAllElements();  // clear the old vector
-            repaint();
-        }
-            
-        public void createWaveForm(byte[] audioBytes) {
-
-            lines.removeAllElements();  // clear the old vector
-
-            AudioFormat format = audioInputStream.getFormat();
-            if (audioBytes == null) {
-                try {
-                    audioBytes = new byte[
-                        (int) (audioInputStream.getFrameLength() 
-                        * format.getFrameSize())];
-                    audioInputStream.read(audioBytes);
-                } catch (Exception ex) { 
-                    reportStatus(ex.toString());
-                    return; 
-                }
-            }
-
-            Dimension d = getSize();
-            int w = d.width;
-            int h = d.height-15;
-            int[] audioData = null;
-            int audioDatum = 0; // Will store values one-by-one
-            long numPeakValues = 0; // To help detect peaking
-            int peakThresh = (int)(Math.pow(2, format.getSampleSizeInBits()) * 0.4);
- //           long nlengthInSamples = ais.getFrameLength() * ais.getFormat().getChannels();
-            int nlengthInSamples =0;
-            if (format.getSampleSizeInBits() == 16) {
-                // int nlengthInSamples = audioBytes.length / 2;
-                 nlengthInSamples = audioBytes.length / 2;
-                 audioData = new int[nlengthInSamples];
-                 if (format.isBigEndian()) {
-                    for (int i = 0; i < nlengthInSamples; i++) {
-                         /* First byte is MSB (high order) */
-                         int MSB = audioBytes[2*i];
-                         /* Second byte is LSB (low order) */
-                         int LSB = audioBytes[2*i+1];
-                         audioData[i] = MSB << 8 | (255 & LSB);
-						 audioDatum = MSB << 8 | (255 & LSB);
-						 if(audioDatum > peakThresh) {
-							 numPeakValues++;
-						 }
-                     }
-                 } else {
-                     for (int i = 0; i < nlengthInSamples; i++) {
-                         /* First byte is LSB (low order) */
-                         int LSB = audioBytes[2*i];
-                         /* Second byte is MSB (high order) */
-                         int MSB = audioBytes[2*i+1];
-                         audioData[i] = MSB << 8 | (255 & LSB);
-						 audioDatum = MSB << 8 | (255 & LSB);
-						 if(audioDatum > peakThresh) {
-							 numPeakValues++;
-						 }
-                     }
-                 }
-             } 
-               
-            int frames_per_pixel = audioBytes.length / format.getFrameSize()/w;
-            byte my_byte = 0;
-            double y_last = 0;
-            int numChannels = format.getChannels();
-            for (double x = 0; x < w && audioData != null; x++) {
-                int idx = (int) (frames_per_pixel * numChannels * x);
-                if (format.getSampleSizeInBits() == 8) {
-                     my_byte = (byte) audioData[idx];
-                } else {
-                     my_byte = (byte) (128 * audioData[idx] / 32768 );
-                }
-                double y_new = h * (128 - my_byte) / 256;
-                lines.add(new Line2D.Double(x, y_last, x, y_new));
-                y_last = y_new;
-            }
-            System.err.println("numPeakValues: " + numPeakValues);
-            float proportionPeakValues = ((float)numPeakValues) / (nlengthInSamples);
-            System.err.println("proportionPeakValues: " + proportionPeakValues);
-            peakWarning = proportionPeakValues > 0.001f;
-            repaint();
-        }
-
-        @Override
-		public void paint(Graphics g) {
-
-            Dimension d = getSize();
-            int w = d.width;
-            int h = d.height;
-            int INFOPAD = 15;
-
-            Graphics2D g2 = (Graphics2D) g;
-            g2.setBackground(getBackground());
-            g2.clearRect(0, 0, w, h);
-            g2.setColor(Color.white);
-            g2.fillRect(0, h-INFOPAD, w, INFOPAD);
-
-            if (errStr != null) {
-                g2.setColor(jfcBlue);
-                g2.setFont(new Font("serif", Font.BOLD, 18));
-                g2.drawString("ERROR", 5, 20);
-                AttributedString as = new AttributedString(errStr);
-                as.addAttribute(TextAttribute.FONT, font12, 0, errStr.length());
-                AttributedCharacterIterator aci = as.getIterator();
-                FontRenderContext frc = g2.getFontRenderContext();
-                LineBreakMeasurer lbm = new LineBreakMeasurer(aci, frc);
-                float x = 5, y = 25;
-                lbm.setPosition(0);
-                while (lbm.getPosition() < errStr.length()) {
-                    TextLayout tl = lbm.nextLayout(w-x-5);
-                    if (!tl.isLeftToRight()) {
-                        x = w - tl.getAdvance();
-                    }
-                    tl.draw(g2, x, y += tl.getAscent());
-                    y += tl.getDescent() + tl.getLeading();
-                }
-            } else if (capture.thread != null) {
-                g2.setColor(Color.black);
-                g2.setFont(font12);
-                g2.drawString(sampleGraphLengthLabel + String.valueOf(seconds), 3, h-4);
-            } else {
-                g2.setColor(Color.black);
-                g2.setFont(font12);
-                g2.drawString(sampleGraphFileLabel + fileName + sampleGraphLengthLabel + String.valueOf(duration) + sampleGraphPositionLabel + String.valueOf(seconds), 3, h-4);
-
-                if (audioInputStream != null) {
-                    // .. render sampling graph ..
-                    g2.setColor(jfcBlue);
-                    for (int i = 1; i < lines.size(); i++) {
-                        g2.draw((Line2D) lines.get(i));
-                    }
-
-                    // .. draw current position ..
-                    if (seconds != 0) {
-                        double loc = seconds/duration*w;
-                        g2.setColor(pink);
-                        g2.setStroke(new BasicStroke(3));
-                        g2.draw(new Line2D.Double(loc, 0, loc, h-INFOPAD-2));
-                    }
-                }
-            }
-        }
-    
-        public void start() {
-            thread = new Thread(this);
-            thread.setName("SamplingGraph");
-            thread.start();
-            seconds = 0;
-        }
-
-        public void stop() {
-            if (thread != null) {
-                thread.interrupt();
-            }
-            thread = null;
-        }
-
-		public void run() {
-            seconds = 0;
-            while (thread != null) {
-                if ((playback.line != null) && (playback.line.isOpen()) ) {
-
-                    long milliseconds = playback.line.getMicrosecondPosition() / 1000;
-
-                    seconds =  milliseconds / 1000.0;
-                } else if ( (capture.line != null) && (capture.line.isActive()) ) {
-
-                    long milliseconds = capture.line.getMicrosecondPosition() / 1000;
-                    seconds =  milliseconds / 1000.0;
-
-                }
-
-                try { Thread.sleep(100); } catch (Exception e) { break; }
-                
-                repaint();
-                                
-                while ((capture.line != null && !capture.line.isActive()) ||
-                       (playback.line != null && !playback.line.isOpen())) 
-                {
-                    try { Thread.sleep(10); } catch (Exception e) { break; }
-                }
-            }
-            seconds = 0;
-            repaint();
-        }
-    } // End class SamplingGraph
 
     /**
      * always called by Postlet uploader
---
+++ src/speechrecorder/SamplingGraph.java
@@ -0,0 +1,279 @@
+package speechrecorder;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.font.FontRenderContext;
+import java.awt.font.LineBreakMeasurer;
+import java.awt.font.TextAttribute;
+import java.awt.font.TextLayout;
+import java.awt.geom.Line2D;
+import java.awt.geom.Line2D.Double;
+import java.text.AttributedCharacterIterator;
+import java.text.AttributedString;
+import java.util.Vector;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.swing.JPanel;
+
+import speechrecorder.CapturePlayback.Capture;
+import speechrecorder.CapturePlayback.Playback;
+
+/**
+ * Render a WaveForm.
+ */
+@SuppressWarnings("serial")
+class SamplingGraph extends JPanel implements Runnable {
+    private Thread thread;
+    private final Font font12 = new Font("serif", Font.PLAIN, 12);
+    Color jfcBlue = new Color(204, 204, 255);
+    Color pink = new Color(255, 175, 175);
+    protected boolean peakWarning = false;
+    
+    // constructor vars
+    CapturePlayback capturePlayback;
+	Vector<Double> lines = new Vector<Double>();
+	AudioInputStream audioInputStream;    
+	Capture capture;
+    Playback playback;
+
+    // paint vars
+    String sampleGraphFileLabel;
+    String fileName;
+	String sampleGraphLengthLabel;
+	double duration;
+	String sampleGraphPositionLabel;
+    
+    double seconds;	
+	String errStr;
+
+    public SamplingGraph (
+    		CapturePlayback capturePlayback,
+    		Vector<Double> lines,
+    		Capture capture,
+    		Playback playback
+    		) 
+    {
+    	setBackground(new Color(20, 20, 20));
+        this.capturePlayback = capturePlayback;
+        this.lines = lines;
+        this.capture = capture;
+        this.playback = playback;
+    }   
+    
+    public void createWaveForm(AudioInputStream audioInputStream, byte[] audioBytes) {
+
+        lines.removeAllElements();  // clear the old vector
+
+        AudioFormat format = audioInputStream.getFormat();
+        if (audioBytes == null) {
+            try {
+                audioBytes = new byte[
+                    (int) (audioInputStream.getFrameLength() 
+                    * format.getFrameSize())];
+                audioInputStream.read(audioBytes);
+            } catch (Exception ex) { 
+            	reportStatus(ex.toString());
+                return; 
+            }
+        }
+
+        Dimension d = getSize();
+        int w = d.width;
+        int h = d.height-15;
+        int[] audioData = null;
+        int audioDatum = 0; // Will store values one-by-one
+        long numPeakValues = 0; // To help detect peaking
+        int peakThresh = (int)(Math.pow(2, format.getSampleSizeInBits()) * 0.4);
+//           long nlengthInSamples = ais.getFrameLength() * ais.getFormat().getChannels();
+        int nlengthInSamples =0;
+        if (format.getSampleSizeInBits() == 16) {
+            // int nlengthInSamples = audioBytes.length / 2;
+             nlengthInSamples = audioBytes.length / 2;
+             audioData = new int[nlengthInSamples];
+             if (format.isBigEndian()) {
+                for (int i = 0; i < nlengthInSamples; i++) {
+                     /* First byte is MSB (high order) */
+                     int MSB = audioBytes[2*i];
+                     /* Second byte is LSB (low order) */
+                     int LSB = audioBytes[2*i+1];
+                     audioData[i] = MSB << 8 | (255 & LSB);
+					 audioDatum = MSB << 8 | (255 & LSB);
+					 if(audioDatum > peakThresh) {
+						 numPeakValues++;
+					 }
+                 }
+             } else {
+                 for (int i = 0; i < nlengthInSamples; i++) {
+                     /* First byte is LSB (low order) */
+                     int LSB = audioBytes[2*i];
+                     /* Second byte is MSB (high order) */
+                     int MSB = audioBytes[2*i+1];
+                     audioData[i] = MSB << 8 | (255 & LSB);
+					 audioDatum = MSB << 8 | (255 & LSB);
+					 if(audioDatum > peakThresh) {
+						 numPeakValues++;
+					 }
+                 }
+             }
+         } 
+           
+        int frames_per_pixel = audioBytes.length / format.getFrameSize()/w;
+        byte my_byte = 0;
+        double y_last = 0;
+        int numChannels = format.getChannels();
+        for (double x = 0; x < w && audioData != null; x++) {
+            int idx = (int) (frames_per_pixel * numChannels * x);
+            if (format.getSampleSizeInBits() == 8) {
+                 my_byte = (byte) audioData[idx];
+            } else {
+                 my_byte = (byte) (128 * audioData[idx] / 32768 );
+            }
+            double y_new = h * (128 - my_byte) / 256;
+            lines.add(new Line2D.Double(x, y_last, x, y_new));
+            y_last = y_new;
+        }
+        System.err.println("numPeakValues: " + numPeakValues);
+        float proportionPeakValues = ((float)numPeakValues) / (nlengthInSamples);
+        System.err.println("proportionPeakValues: " + proportionPeakValues);
+        peakWarning = proportionPeakValues > 0.001f;
+        
+        repaint(); 
+    }
+
+	public void repaintGraph(
+    		AudioInputStream audioInputStream,
+			String sampleGraphFileLabel,
+		    String fileName,
+			String sampleGraphLengthLabel,
+			double duration,
+			String sampleGraphPositionLabel
+			
+		) 
+    {
+		
+		this.sampleGraphFileLabel = sampleGraphFileLabel;
+		this.fileName = fileName;
+		this.sampleGraphLengthLabel = sampleGraphLengthLabel;
+		this.duration = duration;
+		this.sampleGraphPositionLabel = sampleGraphPositionLabel;
+		this.audioInputStream = audioInputStream;
+		
+		repaint(); // calls paint
+    }
+    
+    @Override
+	public void paint(Graphics g) 
+    {
+        Dimension d = getSize();
+        int w = d.width;
+        int h = d.height;
+        int INFOPAD = 15;
+
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setBackground(getBackground());
+        g2.clearRect(0, 0, w, h);
+        g2.setColor(Color.white);
+        g2.fillRect(0, h-INFOPAD, w, INFOPAD);
+
+        if (errStr != null) {
+            g2.setColor(jfcBlue);
+            g2.setFont(new Font("serif", Font.BOLD, 18));
+            g2.drawString("ERROR", 5, 20);
+            AttributedString as = new AttributedString(errStr);
+            as.addAttribute(TextAttribute.FONT, font12, 0, errStr.length());
+            AttributedCharacterIterator aci = as.getIterator();
+            FontRenderContext frc = g2.getFontRenderContext();
+            LineBreakMeasurer lbm = new LineBreakMeasurer(aci, frc);
+            float x = 5, y = 25;
+            lbm.setPosition(0);
+            while (lbm.getPosition() < errStr.length()) {
+                TextLayout tl = lbm.nextLayout(w-x-5);
+                if (!tl.isLeftToRight()) {
+                    x = w - tl.getAdvance();
+                }
+                tl.draw(g2, x, y += tl.getAscent());
+                y += tl.getDescent() + tl.getLeading();
+            }
+        } else if (capture.thread != null) {
+            g2.setColor(Color.black);
+            g2.setFont(font12);
+            g2.drawString(sampleGraphLengthLabel + String.valueOf(seconds), 3, h-4);
+        } else {
+            g2.setColor(Color.black);
+            g2.setFont(font12);
+            g2.drawString(sampleGraphFileLabel + fileName + sampleGraphLengthLabel + String.valueOf(duration) + sampleGraphPositionLabel + String.valueOf(seconds), 3, h-4);
+
+            if (audioInputStream != null) {
+                // .. render sampling graph ..
+                g2.setColor(jfcBlue);
+                for (int i = 1; i < lines.size(); i++) {
+                    g2.draw((Line2D) lines.get(i));
+                }
+
+                // .. draw current position ..
+                if (seconds != 0) {
+                    double loc = seconds/duration*w;
+                    g2.setColor(pink);
+                    g2.setStroke(new BasicStroke(3));
+                    g2.draw(new Line2D.Double(loc, 0, loc, h-INFOPAD-2));
+                }
+            }
+        }
+    }
+
+    public void start() {
+        thread = new Thread(this);
+        thread.setName("SamplingGraph");
+        thread.start();
+        seconds = 0;
+    }
+
+    public void stop() {
+        if (thread != null) {
+            thread.interrupt();
+        }
+        thread = null;
+    }
+
+	public void run() {
+        seconds = 0;
+        while (thread != null) {
+            if ((playback.line != null) && (playback.line.isOpen()) ) {
+
+                long milliseconds = playback.line.getMicrosecondPosition() / 1000;
+
+                seconds =  milliseconds / 1000.0;
+            } else if ( (capture.line != null) && (capture.line.isActive()) ) {
+
+                long milliseconds = capture.line.getMicrosecondPosition() / 1000;
+                seconds =  milliseconds / 1000.0;
+
+            }
+
+            try { Thread.sleep(100); } catch (Exception e) { break; }
+            
+            repaint();
+                            
+            while ((capture.line != null && !capture.line.isActive()) ||
+                   (playback.line != null && !playback.line.isOpen())) 
+            {
+                try { Thread.sleep(10); } catch (Exception e) { break; }
+            }
+        }
+        seconds = 0;
+        repaint();
+    }
+	
+    private void reportStatus(String msg) {
+        if ((errStr = msg) != null) {
+            System.out.println(errStr);
+            this.repaint();
+        }
+    }
+} 
+
